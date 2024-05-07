37196a9ceba07c029dd76258ca8109417a656adc
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-07 10:16:46 +0200

    [UPD] snack 9

---
+++ js-snacks9/js/myscript.js
@@ -0,0 +1,11 @@
+'use strict';
+let numeri=[0,1,2,3,4,5,6,7,8,9];
+let somma=0;
+for(let i=0; i<10; i++){
+    somma= somma+numeri[i];
+}
+console.log(somma);
+
+const media=somma/10;
+
+console.log(media);
